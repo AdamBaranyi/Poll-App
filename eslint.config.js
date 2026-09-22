e7c8802cff7c155d42577f0e547e46f2022839cf
Adam Baranyi <baranyiadam27@gmail.com>
2026-09-22 13:29:06 +0200

    Enforce coding conventions with ESLint rules

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ const eslint = require('@eslint/js');
 const { defineConfig } = require('eslint/config');
 const tseslint = require('typescript-eslint');
 const angular = require('angular-eslint');
+const jsdoc = require('eslint-plugin-jsdoc');
 
 module.exports = defineConfig([
   {
@@ -14,6 +15,7 @@ module.exports = defineConfig([
       angular.configs.tsRecommended,
     ],
     processor: angular.processInlineTemplates,
+    plugins: { jsdoc },
     rules: {
       '@angular-eslint/directive-selector': [
         'error',
@@ -31,11 +33,45 @@ module.exports = defineConfig([
           style: 'kebab-case',
         },
       ],
+      'max-lines-per-function': ['error', { max: 14, skipBlankLines: true, skipComments: true }],
+      'max-lines': ['error', { max: 400 }],
+      'no-console': ['error', { allow: ['error'] }],
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/explicit-function-return-type': ['error', { allowExpressions: true }],
+      'no-magic-numbers': 'off',
+      '@typescript-eslint/no-magic-numbers': [
+        'error',
+        { ignore: [-1, 0, 1], ignoreEnums: true, ignoreReadonlyClassProperties: true },
+      ],
+      '@typescript-eslint/naming-convention': [
+        'error',
+        { selector: 'default', format: ['camelCase'], leadingUnderscore: 'allow' },
+        { selector: 'variable', modifiers: ['const'], format: ['camelCase', 'UPPER_CASE'] },
+        { selector: 'typeLike', format: ['PascalCase'] },
+        { selector: 'enumMember', format: ['PascalCase'] },
+        { selector: ['objectLiteralProperty', 'typeProperty'], format: ['camelCase', 'snake_case'] },
+        { selector: 'property', modifiers: ['requiresQuotes'], format: null },
+        { selector: 'import', format: null },
+      ],
+      'jsdoc/require-jsdoc': [
+        'error',
+        {
+          require: {
+            FunctionDeclaration: true,
+            MethodDefinition: true,
+            ArrowFunctionExpression: true,
+          },
+          checkConstructors: false,
+        },
+      ],
     },
   },
   {
     files: ['**/*.html'],
     extends: [angular.configs.templateRecommended, angular.configs.templateAccessibility],
-    rules: {},
+    rules: {
+      '@angular-eslint/template/prefer-control-flow': 'error',
+      'max-lines': ['error', { max: 400 }],
+    },
   },
 ]);
