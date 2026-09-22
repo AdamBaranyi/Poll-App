81d55dfce034dfed1847f8b65764560d80ab963e
Adam Baranyi <baranyiadam27@gmail.com>
2026-09-22 14:37:28 +0200

    Add the start page hero with the illustration

---
+++ src/app/features/home/hero/hero.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero',
+  styleUrl: './hero.component.scss',
+  templateUrl: './hero.component.html',
+})
+export class HeroComponent {}
