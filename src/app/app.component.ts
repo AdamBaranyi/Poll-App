7eeb9d0c0505d3171cad22e46b9eb7b5949b33be
Adam Baranyi <baranyiadam27@gmail.com>
2026-09-22 13:27:45 +0200

    Name classes with their Angular type suffix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,6 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss',
   templateUrl: './app.component.html',
 })
-export class App {
+export class AppComponent {
   protected readonly title = signal('poll-app');
 }
