5b1042c50a87fe245a6b141841b3be731793edd2
Adam Baranyi <baranyiadam27@gmail.com>
2026-09-22 18:41:02 +0200

    Load the create survey dialog on first use and lock the page scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { SurveyDialogService } from './core/services/survey-dialog.service';
+import { CreateSurveyComponent } from './features/create-survey/create-survey.component';
+
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CreateSurveyComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent {
+  protected readonly dialogState = inject(SurveyDialogService);
+}
