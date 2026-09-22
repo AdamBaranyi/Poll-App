fe855d473455e0eb95f91ab42e3b05ca43b09dd8
Adam Baranyi <baranyiadam27@gmail.com>
2026-09-22 13:40:23 +0200

    Add home and survey detail pages with routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './features/home/home.component';
+import { SurveyDetailComponent } from './features/survey-detail/survey-detail.component';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent, title: 'Poll App' },
+  { path: 'survey/:id', component: SurveyDetailComponent, title: 'Survey | Poll App' },
+  { path: '**', redirectTo: '' },
+];
---
+++ src/app/features/survey-detail/survey-detail.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-survey-detail',
+  styleUrl: './survey-detail.component.scss',
+  templateUrl: './survey-detail.component.html',
+})
+export class SurveyDetailComponent {}
